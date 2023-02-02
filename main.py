28a48aa2bfcf260d1db51f74e2abd496a1393b27
Tushar Mishra <tushar.mishra@objectedge.com>
2023-02-02 16:35:04 +0530

    refactored the codebase

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ database_name = config['database']['database_name']
 # Create a connection to the database
 engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database_name}', echo=0)
 
-# Create a session
-session = Session(bind=engine)
-
 
 class Base(DeclarativeBase):
     pass
@@ -38,32 +35,34 @@ class Customer(Base):
         return f'table - customer p-key[{self.first_name} {self.last_name}]'
 
 
-# create operation
-session.execute(
-    insert(Customer),
-    [
-        {"first_name": "object", "last_name": "edge", "dob": "15.07.2000", "address": "navi mumbai",
-         "created_by": "2018-09-24T00:00:00", "updated_by": "2019-09-24T00:00:00"},
-        {'first_name': 'O', 'last_name': 'E', 'dob': '15/07/1994', 'address': 'thane',
-         'created_by': '2018-09-24T00:00:00', 'updated_by': '2019-09-24T10:20:30'},
-    ])
+# creating an enclosing session
+with Session(bind=engine) as session:
+    # create operation
+    session.execute(
+        insert(Customer),
+        [
+            {"first_name": "object", "last_name": "edge", "dob": "15.07.2000", "address": "navi mumbai",
+             "created_by": "2018-09-24T00:00:00", "updated_by": "2019-09-24T00:00:00"},
+            {'first_name': 'O', 'last_name': 'E', 'dob': '15/07/1994', 'address': 'thane',
+             'created_by': '2018-09-24T00:00:00', 'updated_by': '2019-09-24T10:20:30'},
+        ])
 
-# read operation
-res = session.scalars(select(Customer))
+    # read operation
+    res = session.scalars(select(Customer))
 
-for user in res:
-    print(user)
+    for user in res:
+        print(user)
 
-# update operation
-session.execute(
-    update(Customer),
-    [
-        {'first_name': 'O', 'last_name': 'E', 'dob': '31/07/1994', 'address': 'Walnut Creek, United States',
-         'created_by': '1994-07-31T00:00:00', 'updated_by': '2023-09-24T10:20:30'},
-    ])
+    # update operation
+    session.execute(
+        update(Customer),
+        [
+            {'first_name': 'O', 'last_name': 'E', 'dob': '31/07/1994', 'address': 'Walnut Creek, United States',
+             'created_by': '1994-07-31T00:00:00', 'updated_by': '2023-09-24T10:20:30'},
+        ])
 
-# delete operation
-session.execute(delete(Customer).where(Customer.first_name.in_(["object", 'O'])))
+    # delete operation
+    session.execute(delete(Customer).where(Customer.first_name.in_(["object", 'O'])))
 
-# committing changes
-session.commit()
+    # committing changes
+    session.commit()
