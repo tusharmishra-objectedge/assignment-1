73a4fe539f6d60f3278a211c34bbbb9beea990a0
Tushar Mishra <tushar.mishra@objectedge.com>
2023-02-02 16:29:32 +0530

    added try except blocks

--- main.py
+++ main.py
@@ -37,22 +37,49 @@ class Customer(Base):
         return f'table - customer p-key[{self.first_name} {self.last_name}]'
 
 
-# create operation
-session.execute(text(
-    "INSERT INTO customer (first_name, last_name, dob, address, created_by, updated_by) VALUES ('object', 'edge', '15.07.2000', 'navi mumbai', '2018-09-24T00:00:00', '2019-09-24T00:00:00'), ('O', 'E', '15/07/1994', 'thane', '2018-09-24T00:00:00', '2019-09-24T10:20:30')"))
+try:
+    # create operation
+    try:
+        session.execute(text(
+            "INSERT INTO customer (first_name, last_name, dob, address, created_by, updated_by) VALUES ('object', 'edge', '15.07.2000', 'navi mumbai', '2018-09-24T00:00:00', '2019-09-24T00:00:00'), ('O', 'E', '15/07/1994', 'thane', '2018-09-24T00:00:00', '2019-09-24T10:20:30')"))
+    except Exception as e:
+        print("Error during create operation:", e)
+        session.rollback()
 
-# read operation
-res = session.execute(text("SELECT * FROM customer"))
+    # read operation
+    try:
+        res = session.execute(text("SELECT * FROM customer"))
+        for row in res:
+            print(row)
+    except Exception as e:
+        print("Error during read operation:", e)
+        session.rollback()
 
-for row in res:
-    print(row)
+    # update operation
+    try:
+        session.execute(text(
+            "UPDATE customer SET dob='31/07/1994', address='Walnut Creek, United States', created_by='1994-07-31T00:00:00', updated_by='2023-09-24T10:20:30' WHERE first_name='O' and last_name='E'"))
+    except Exception as e:
+        print("Error during update operation:", e)
+        session.rollback()
 
-# update operation
-session.execute(text(
-    "UPDATE customer SET dob='31/07/1994', address='Walnut Creek, United States', created_by='1994-07-31T00:00:00', updated_by='2023-09-24T10:20:30' WHERE first_name='O' and last_name='E'"))
+    # delete operation
+    try:
+        session.execute(text("DELETE FROM customer WHERE first_name in ('object', 'O')"))
+    except Exception as e:
+        print("Error during delete operation:", e)
+        session.rollback()
 
-# delete operation
-session.execute(text("DELETE FROM customer WHERE first_name in ('object', 'O')"))
+    # committing changes
+    try:
+        session.commit()
+    except Exception as e:
+        print("Error during commit:", e)
+        session.rollback()
 
-# committing changes
-session.commit()
+except Exception as e:
+    print("An unknown error occurred:", e)
+    session.rollback()
+
+finally:
+    session.close()
